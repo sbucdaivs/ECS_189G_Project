0ee64a6595a15f0ac52e80b297e79fe0797ab13b
amberchen122 <amberchen2020@gmail.com>
2022-01-26 13:13:22 -0800

    changed some parameter

--- script/stage_2_script/script_mlp.py
+++ script/stage_2_script/script_mlp.py
@@ -1,23 +1,22 @@
-from code.stage_1_code.Dataset_Loader import Dataset_Loader
-from code.stage_1_code.Method_MLP import Method_MLP
-from code.stage_1_code.Result_Saver import Result_Saver
-from code.stage_1_code.Setting_KFold_CV import Setting_KFold_CV
-from code.stage_1_code.Setting_Train_Test_Split import Setting_Train_Test_Split
-from code.stage_1_code.Evaluate_Accuracy import Evaluate_Accuracy
+from code.stage_2_code.Dataset_Loader import Dataset_Loader
+from code.stage_2_code.Method_MLP import Method_MLP
+from code.stage_2_code.Result_Saver import Result_Saver
+from code.stage_2_code.Setting_KFold_CV import Setting_KFold_CV
+from code.stage_2_code.Evaluate_Accuracy import Evaluate_Accuracy
 import numpy as np
 import torch
 
 #---- Multi-Layer Perceptron script ----
 if 1:
     #---- parameter section -------------------------------
-    np.random.seed(2)
+    np.random.seed(2)   # TODO: Why does it need this?
     torch.manual_seed(2)
     #------------------------------------------------------
 
     # ---- objection initialization setction ---------------
-    data_obj = Dataset_Loader('toy', '')  # TODO: where did it load data?
-    data_obj.dataset_source_folder_path = '../../data/stage_1_data/'
-    data_obj.dataset_source_file_name = 'toy_data_file.txt'
+    data_obj = Dataset_Loader('MNIST', 'Handwritten digits.')  # TODO: where did it load data?
+    data_obj.dataset_source_folder_path = '../../data/stage_2_data/'
+    data_obj.dataset_source_file_name = ['train.csv', 'test.csv']
 
     method_obj = Method_MLP('multi-layer perceptron', '')
 
@@ -26,8 +25,7 @@ if 1:
     result_obj.result_destination_file_name = 'prediction_result'
 
     setting_obj = Setting_KFold_CV('k fold cross validation', '')
-    #setting_obj = Setting_Tra
-    # in_Test_Split('train test split', '')
+    #setting_obj = Setting_Train_Test_Split('train test split', '')
 
     evaluate_obj = Evaluate_Accuracy('accuracy', '')
     # ------------------------------------------------------
@@ -41,6 +39,7 @@ if 1:
     print('MLP Accuracy: ' + str(mean_score) + ' +/- ' + str(std_score))
     print('************ Finish ************')
     # ------------------------------------------------------
+    # TODO: add testing on test.csv
     
 
     
