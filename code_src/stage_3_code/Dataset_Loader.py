f3205b2646b339d37b93d1b6032b38c4de2d6c07
amberchen122 <amberchen2020@gmail.com>
2022-02-08 13:44:27 -0800

    data loader and viewer

---
+++ code_src/stage_3_code/Dataset_Loader.py
@@ -0,0 +1,21 @@
+import pickle
+import matplotlib.pyplot as plt
+
+from code_src.base_class.dataset import dataset
+
+
+class Dataset_Loader(dataset):
+    data = None
+    dataset_source_folder_path = None
+    dataset_source_file_name = None
+
+    def __init__(self, dName=None, dDescription=None):
+        super().__init__(dName, dDescription)
+
+    def load(self):
+        print('loading data...')
+        f = open(self.dataset_source_folder_path + self.dataset_source_file_name, 'rb')
+        # f = open('../../data/stage_3_data/MNIST', 'rb')
+        data = pickle.load(f)
+        f.close()
+        return data
---
+++ code_src/stage_3_code/Dataset_Viewer.py
@@ -0,0 +1,15 @@
+import pickle
+import matplotlib.pyplot as plt
+
+if 1:
+   f = open('../../data/stage_3_data/MNIST', 'rb') # or change MNIST to other dataset names
+   data = pickle.load(f)
+   f.close()
+
+   print('training set size:', len(data['train']), 'testing set size:', len(data['test']))
+
+   for pair in data['train']:
+   #for pair in data['test']:
+       plt.imshow(pair['image'], cmap="Greys")
+       plt.show()
+       print(pair['label'])
